d46a013df2905f70b2c75ceeec3e72937029600b
CleverNimbus <86955324+CleverNimbus@users.noreply.github.com>
2024-07-29 14:16:03 +0200

    Updates

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-/*!
-* Start Bootstrap - CleverNimbus v1.0.0 (https://CleverNimbus.github.io)
-* Copyright 2013-2024 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/clevernimbus-landing-page/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
